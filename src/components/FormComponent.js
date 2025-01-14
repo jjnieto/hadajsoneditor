306616c83098b32f637e73d8618a2e58ff6be9a8
Javier Nieto <jjnieto@gmail.com>
2025-01-14 16:58:46 +0100

    schema

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -2,16 +2,17 @@ import Form from "@rjsf/core";
 import React from "react";
 
 function FormComponent() {
-    const schema = require("../jsonFiles/schema.json")
-    const formData = require("../jsonFiles/formData.json")
-    const uiSchema = require("../jsonFiles/uiSchema.json")
+    //const schema = require("../jsonFiles/schema.json")
+    const schema = require("../jsonFiles/haadaschema.json")
+    //const formData = require("../jsonFiles/formData.json")
+    //const uiSchema = require("../jsonFiles/uiSchema.json")
 
     return (
         <div className="form">
             <Form
                 schema={schema}
-                uiSchema={uiSchema}
-                formData={formData}
+                //uiSchema={uiSchema}
+                //formData={formData}
             >
 
             </Form>
